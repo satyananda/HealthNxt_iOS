7f0658279967d47a484d95fb4f74fc2840b22715
Satyanand <satyanand.t@gmail.com>
2020-08-17 13:08:20 +0530

    initial Project

---
+++ HealthNxtApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthNxtApp
+//
+//  Created by Satyananda Kumar on 17/08/20.
+//  Copyright © 2020 Satyananda Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
